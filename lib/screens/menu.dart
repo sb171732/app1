a7ba7003bf012258a2cb13e7aee09e035438d6c5
Mengi <osnovanskii@gmail.com>
2023-10-20 21:47:38 +0700

    выхожу за рамки

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
+=======
+import 'package:flutter/widgets.dart';
+>>>>>>> e4bd454 (добавил меню и jsondecode)
 import 'package:http/http.dart' as http;
 
 class Menu extends StatefulWidget {
@@ -7,6 +11,7 @@ class Menu extends StatefulWidget {
 
   @override
   State<Menu> createState() => _MenuState();
+<<<<<<< HEAD
   
 }
 
@@ -22,6 +27,14 @@ void initState() {
 
   Future <void> loadMenu() async {
     final response = await http.get(Uri.parse('http://192.168.96.55:5000/menu'),);
+=======
+}
+
+class _MenuState extends State<Menu> {
+  List<Map<String, dynamic>> menu = [];
+  Future <void> loadMenu() async {
+    final response = await http.get(Uri.parse('ZATOCHKA'),);
+>>>>>>> e4bd454 (добавил меню и jsondecode)
     if (response.statusCode==200){
       final List<Map<String, dynamic>> jsonData = List<Map<String,dynamic>>.from(jsonDecode(response.body),);
       setState(() {
@@ -31,12 +44,15 @@ void initState() {
       throw Exception('Ошибка');
     }
   }
+<<<<<<< HEAD
     void addToCart(Map<String,dynamic> item){
     setState(() {
     cart.add(item);
     });
     print("добавляю в корзину");
   }
+=======
+>>>>>>> e4bd454 (добавил меню и jsondecode)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +63,11 @@ void initState() {
           return ListTile(
             leading: InkWell(onTap: (){}, child: Image.network(dish['image'],),),
             title: InkWell(onTap: (){}, child: Text(dish['name'],),),
+<<<<<<< HEAD
             subtitle: TextButton(onPressed: (){addToCart(dish);}, child: Text(dish['price'].toString(),),
+=======
+            subtitle: TextButton(onPressed: (){}, child: Text(dish['price'],),
+>>>>>>> e4bd454 (добавил меню и jsondecode)
             ),
           );
         }
