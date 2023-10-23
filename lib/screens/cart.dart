f373f71f423ba9930fb886be8700c57df974de52
Mengi <osnovanskii@gmail.com>
2023-10-23 14:58:23 +0700

    Создание корзины

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:app1/screens/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,26 +13,68 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  
+  
   @override
   Widget build(BuildContext context) {
     final cartState = Provider.of<CartState>(context);
-
-    return Scaffold(
-      body: ListView.builder(
+    return cartState.cart.isEmpty ? Scaffold(
+      body: Center(
+        child: Text("Корзина пуста"),
+      ),
+      
+    ) 
+    :
+    Scaffold(
+      body: Column(
+        children: [
+          Expanded(child:ListView.builder(
         itemCount: cartState.cart.length,
         itemBuilder: (context, index) {
           final dish = cartState.cart[index];
           return ListTile(
-            leading: InkWell(onTap: () {}, child: Image.network(dish['image'])),
-            title: InkWell(onTap: () {}, child: Text(dish['name'])),
+            leading: InkWell(onTap: () {Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => AnimatedDishDetails(dish: dish, isFromCart: true),
+                ));}, child: Image.network(dish['image'])),
+            title: InkWell(onTap: () {Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => AnimatedDishDetails(dish: dish, isFromCart: true),
+                ));}, child: Text(dish['name'])),
             subtitle: TextButton(
               onPressed: () {},
               child: Text(dish['price'].toString()),
             ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(onPressed: (){cartState.updateQuantity(dish, 1);}, icon: Icon(Icons.add)),
+                Text(dish['quantity'].toString(), style: TextStyle(fontSize: 16)),
+
+                IconButton(onPressed: (){cartState.updateQuantity(dish, -1);}, icon: Icon(Icons.remove)),
+                
+              ],
+            
+            ),
+            
           );
+          
         },
+
+      ), 
+      
       ),
+        if (cartState.cart.isNotEmpty) 
+          ElevatedButton(
+            onPressed: () {
+              
+            },
+            child: Text('Оплатить${cartState.getTotalPrice().toStringAsFixed(2)}'),
+          ),
+        ],
+      )
+      
+      
     );
+    
   }
 }
 
@@ -39,8 +84,54 @@ class CartState with ChangeNotifier {
 
   List<Map<String, dynamic>> get cart => _cart;
 
+  double getTotalPrice() {
+    double totalPrice = 0.0;
+
+    for (final item in _cart) {
+      final quantity = item['quantity'];
+      final price = item['price'];
+      totalPrice += quantity * price;
+    }
+
+    return totalPrice;
+  }
+
   void addToCart(Map<String, dynamic> item) {
+  final id = item['id'];
+  final existingCartItem = _cart.firstWhere(
+    (cartItem) => cartItem['id'] == id,
+    orElse: () => Map<String, dynamic>.from({}), // Создайте пустой Map вместо null
+  );
+
+  if (existingCartItem != null) {
+    print("обновляю товар ");
+    existingCartItem['quantity'] = (existingCartItem['quantity'] ?? 0) + 1;
+    
+  } else {
+    print(" добавляю товар");
+    item['quantity'] = 1;
     _cart.add(item);
-    notifyListeners();
+  }
+  notifyListeners();
+}
+
+
+  void updateQuantity(Map<String, dynamic> item, int change) {
+    final id = item['id'];
+
+    for (int i = 0; i < _cart.length; i++) {
+      if (_cart[i]['id'] == id) {
+        final newQuantity = _cart[i]['quantity'] + change;
+        if (newQuantity <= 0) {
+          _cart.removeAt(i);
+        } else {
+          _cart[i]['quantity'] = newQuantity;
+        }
+        notifyListeners();
+        break;
+      }
+    }
   }
 }
+
+
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,48 +1,45 @@
 import 'dart:convert';
-import 'package:app1/screens/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
-// Замените на путь к вашему классу состояния корзины
+import 'cart.dart'; // Убедитесь, что путь к вашему файлу cart.dart указан правильно.
 
 class Menu extends StatefulWidget {
   const Menu({Key? key});
-  
 
   @override
   State<Menu> createState() => _MenuState();
-  
 }
 
 class _MenuState extends State<Menu> {
-  List<Map<String, dynamic>> menu = [];
+  List<Dish> menu = []; // Теперь список содержит объекты Dish
+  late CartState cartState;
 
   @override
   void initState() {
     super.initState();
+    cartState = Provider.of<CartState>(context, listen: false);
     loadMenu();
   }
 
   Future<void> loadMenu() async {
-    final response = await http.get(Uri.parse('http://192.168.96.55:5000/menu'),);
+    final response = await http.get(Uri.parse('http://192.168.40.55:5000/menu'),);
     if (response.statusCode == 200) {
-      final List<Map<String, dynamic>> jsonData = List<Map<String, dynamic>>.from(jsonDecode(response.body));
-
-      final cartState = Provider.of<CartState>(context, listen: false);
+      final jsonData = List<Map<String, dynamic>>.from(jsonDecode(response.body));
 
       setState(() {
-        menu = jsonData;
+        menu = jsonData.map((data) => Dish(
+          id: data['id'],
+          name: data['name'],
+          description: data['description'],
+          price: data['price'].toDouble(),
+          image: data['image'],
+        )).toList();
       });
     } else {
       print(Exception);
       throw Exception('Ошибка');
     }
-  }
-
-  void addToCart(Map<String, dynamic> item) {
-    final cartState = Provider.of<CartState>(context, listen: false);
-    cartState.addToCart(item);
-    print("добавляю в корзину");
   }
 
   @override
@@ -53,17 +50,181 @@ class _MenuState extends State<Menu> {
         itemBuilder: (context, index) {
           final dish = menu[index];
           return ListTile(
-            leading: InkWell(onTap: () {}, child: Image.network(dish['image'])),
-            title: InkWell(onTap: () {}, child: Text(dish['name'])),
+            leading: InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                 builder: (context) => AnimatedDishDetails(dish: dish.toMap(), isFromCart: false),
+
+                ));
+              },
+              child: Image.network(dish.image),
+            ),
+            title: InkWell(
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                 builder: (context) => AnimatedDishDetails(dish: dish.toMap(), isFromCart: false),
+
+                ));
+              },
+              child: Text(dish.name),
+            ),
             subtitle: TextButton(
               onPressed: () {
-                addToCart(dish);
+                final cartState = Provider.of<CartState>(context, listen: false);
+                cartState.addToCart(dish.toMap());
               },
-              child: Text(dish['price'].toString()),
+              child: Text(dish.price.toString()),
             ),
           );
         },
       ),
     );
   }
+}
+
+
+
+
+class DishDetails extends StatelessWidget {
+  final Map<String, dynamic> dish;
+  final bool isFromCart;
+  DishDetails({required this.dish, required this.isFromCart});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(dish['image']),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        '${dish['name']}',
+        style: TextStyle(fontSize: 40),
+      ),
+      Text(
+        "Описание",
+        style: TextStyle(fontSize: 20), // Измените на желаемый размер шрифта
+      ),
+      Text(
+        '${dish['description']}',
+        style: TextStyle(fontSize: 15),
+      ),
+    ],
+  ),
+)
+
+              ],
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: TextButton(
+                onPressed: () {
+                  
+                  if (isFromCart) {
+                    Navigator.pop(context);
+                  } else {
+                    final cartState = Provider.of<CartState>(context, listen: false);
+                  cartState.addToCart(dish);
+                  }
+                  
+                },
+                child: Text(isFromCart ? "Готово" : "Добавить в корзину ${dish["price"]}"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimatedDishDetails extends StatefulWidget {
+  final Map<String, dynamic> dish;
+  final bool isFromCart;
+  AnimatedDishDetails({required this.dish, required this.isFromCart});
+
+  @override
+  _AnimatedDishDetailsState createState() => _AnimatedDishDetailsState();
+}
+
+
+class _AnimatedDishDetailsState extends State<AnimatedDishDetails> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset(0, 1.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOut,
+    ));
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: Scaffold(
+        body: GestureDetector(
+          onVerticalDragUpdate: (details) {
+            if (details.primaryDelta! < -20) {
+              _animationController.reverse().then((_) {
+                Navigator.pop(context);
+              });
+            }
+          },
+          child: DishDetails(dish: widget.dish, isFromCart: widget.isFromCart),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
+class Dish {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final String image;
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'image': image,
+    };
+  }
+  Dish({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
 }
